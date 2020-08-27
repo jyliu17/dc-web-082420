ec4cfc5f8dab4ebc0d62f736374fbd8e69bf7191
Paul Nicholsen <paulnicholsen27@gmail.com>
2020-08-27 09:44:02 -0400

    prelecture 04

--- 03-oop/app.rb
+++ 03-oop/app.rb
@@ -33,9 +33,17 @@ class Cat
         My colors are #{self.colors}." # self is Petey or Ella--whoever we called the method on
     end
 
-    def get_all_cats
-        @@all_cats
+    def say_hi_to_everyone
+        Cat.all.each do |kat|
+            if kat != self
+                puts "Hey #{kat.name}, I'm #{self.name}"
+            end
+        end
+        return nil
     end
+    # def get_all_cats #doesn't make sense
+    #     @@all_cats
+    # end
 
     # class methods
 
@@ -48,19 +56,43 @@ class Cat
         return @@natural_enemies.join(" and ")
     end
 
-    def self.all 
+    def self.all # class method Cat.all
         return @@all_cats
     end
 
+    def all # instance method petey.all ella.all
+
+    end
+
+    def self.floofiest
+        all_cats = self.all
+        all_cats.select do |cat|
+            cat.floofiness > 5
+        end
+    end
+     
+    def self.all_names # ["Ella", "Petey", "Garfield"]
+        # 0. Make an empty array
+        # 0.5 Get all the cats
+        # 1. Go through each cat
+        # 2. Write down their name in an array
+        names = []
+        Cat.all.each do |kat|
+            names << kat.name.upcase
+        end
+        return names
+
+    end
 
 end
 
 # binding.pry
 petey = Cat.new("Petey", 8, ["black", "white"])
-# binding.pry
+# # binding.pry
 ella = Cat.new("Ella", 10, ["black"])
 garfield = Cat.new("Garfield", 2)
 # Cat.is_the_best # "Cats are the best"
 
-# write a class method to get all cats that have a floofiness of 5 or higher
+# write a class method to get all cats that have a floofiness of 5 or higher 
+# hint:  Use Cat.all in your method
 binding.pry
