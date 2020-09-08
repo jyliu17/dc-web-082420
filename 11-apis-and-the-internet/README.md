8744938a6831ce8d3745de8047d57ea378a855a1
Paul Nicholsen <paulnicholsen27@gmail.com>
2020-09-08 11:30:25 -0400

    post lecture

--- 11-apis-and-the-internet/README.md
+++ 11-apis-and-the-internet/README.md
@@ -7,10 +7,17 @@
 * Developer console
 * APIs
 * JSON (string formatted as a hash/object)
+  - JavaScript Object Notation
+
+* Status Codes
+  200s - OK
+  300s - Redirect
+  400 - Client Error (404)
+  500 - Server Error 
 
 ### Deliverables
 
 * Write an application that takes a search term from a user
 * Make a Request to the GoogleBooks API and get back some results
   * https://www.googleapis.com/books/v1/volumes?q=ruby+programming
-* Display the titles, author names, and description for each book
+* Display the titles, author names, and description for first ten books
--- 11-apis-and-the-internet/app.rb
+++ 11-apis-and-the-internet/app.rb
@@ -4,3 +4,48 @@ require 'pry'
 
 GOOGLE_BOOKS_API_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="
 
+
+def get_search_terms
+    puts "What would you like to search for?"
+    search_terms = gets.chomp
+    return search_terms
+end
+
+def make_url(search_params)
+    url = GOOGLE_BOOKS_API_BASE_URL + search_params
+    url.gsub!(/\s/, "+")
+    return url 
+end
+
+def get_json(url)
+    response = RestClient.get(url)
+    json_info = JSON.parse(response)
+    return json_info
+end
+
+def display_books(book_info)
+    book_info.each do |book|
+        puts "Title: #{book['volumeInfo']['title']}"
+        if book['volumeInfo']['authors']
+            puts "Author: #{book['volumeInfo']['authors'].join(" & ")}"
+        else
+            puts "Author Unknown"
+        end
+        puts "Description: #{book['volumeInfo']['description']}"
+        puts "************************"
+    end
+end
+
+def run
+    search_terms = get_search_terms
+    url = make_url(search_terms)
+    json = get_json(url)
+    display_books(json["items"][0..10])
+end
+
+run 
+
+
+
+
+
