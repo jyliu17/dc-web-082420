50f33a0eb6e7980beed4caac2cef90e13c392a5c
Paul Nicholsen <paulnicholsen27@gmail.com>
2020-09-15 10:15:21 -0400

    prelecture 15

---
+++ 15-rails-index-show/taco-tracker/app/controllers/static_controller.rb
@@ -0,0 +1,22 @@
+class StaticController < ApplicationController
+
+    def hello_world
+        # get these things from database
+        # sort those things
+        # log this to a file
+        # email my mom
+        render :hello
+    end
+
+    def about 
+        @cohort_name = "Git Lit"
+        # render :about
+    end
+
+    def favorites 
+        @favorites = ["Bacon Taco", "Spinach Taco", "Cheddar Taco"]
+        # render :favorites
+
+    end
+    
+end
