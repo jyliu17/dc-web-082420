0d82aeab12f4c3694a9dd5e778dad4396bb7e12d
Shannon Nabors <shannon.nabors@gmail.com>
2020-10-15 17:48:08 -0400

    adding first code for Flatdango review

---
+++ 36-Code-Challenge-Review/src/index.js
@@ -0,0 +1,81 @@
+const url = "http://localhost:3000/films"
+const filmsDiv = () => document.querySelector("#films")
+const poster = () => document.querySelector("#poster")
+const title = () => document.querySelector('#title')
+const runtime = () => document.querySelector('#runtime')
+const filmInfo = () => document.querySelector('#film-info')
+const showtime = () => document.querySelector('#showtime')
+const ticketNum = () => document.querySelector('#ticket-num')
+const buttonDiv = () => document.querySelector('#button-div')
+const button = () => document.querySelector('#button')
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchAllMovies()
+    fetchOneMovie(1)
+    button().addEventListener('click', buyTicket)
+})
+
+function fetchAllMovies() {
+    fetch(url)
+    .then(r => r.json())
+    .then(listMovies)
+}
+
+function fetchOneMovie(id) {
+    fetch(`${url}/${id}`)
+    .then(r => r.json())
+    .then(displayMovie)
+}
+
+function listMovies(movies) {
+    movies.forEach(movie => {
+        let filmItem = document.createElement('div')
+        filmItem.className = "film item"
+        filmItem.textContent = movie.title
+        filmItem.addEventListener('click', () => fetchOneMovie(movie.id))
+        filmsDiv().appendChild(filmItem)
+    })
+}
+
+function displayMovie(movie) {
+    title().textContent = movie.title
+    runtime().textContent = `${movie.runtime} minutes`
+    filmInfo().textContent = movie.description
+    showtime().textContent = movie.showtime
+    poster().src = movie.poster
+
+    let ticketsLeft = movie.capacity - movie.tickets_sold
+    ticketNum().textContent = ticketsLeft
+
+    button().dataset.id = movie.id
+    button().dataset.capacity = movie.capacity
+    button().dataset.ticketsSold = movie.tickets_sold
+
+    if (ticketsLeft === 0) {
+        button().className = "ui label"
+        button().textContent = "SOLD OUT"
+    } else {
+        button().className = "ui orange button"
+        button().textContent = "Buy Ticket"
+    }
+
+}
+
+
+function buyTicket() {
+    let ticketsSold = parseInt(button().dataset.ticketsSold)
+    let capacity = parseInt(button().dataset.capacity)
+
+    if (ticketsSold < capacity) {
+
+        fetch(`${url}/${button().dataset.id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({tickets_sold: ticketsSold + 1})
+        }).then(r => r.json())
+        .then(updatedMovie => {
+            displayMovie(updatedMovie)
+        })
+
+    }
+}
